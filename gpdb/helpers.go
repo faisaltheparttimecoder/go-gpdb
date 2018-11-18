9098f7ce170785ad50c3ee9d9963191c2376f653
Faisal Ali <fai28683@gmail.com>
2018-11-18 22:59:42 +0000

    v3.0 quick off
    + Setup a new enhanced logger
    + New command line parser

---
+++ gpdb/helpers.go
@@ -0,0 +1,12 @@
+package main
+
+// Function that checks if the string is available on a array.
+func Contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
