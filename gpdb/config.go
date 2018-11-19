7ce66176b18d42a4809deb0ee45c160fbcb93844
faisaltheparttimecoder <fai28683@gmail.com>
2018-11-19 10:15:29 +0000

    validation of configuration.
    Configuration Validation is complete

--- gpdb/config.go
+++ gpdb/config.go
@@ -32,9 +32,86 @@ var Config = struct {
 	} `yaml:"INSTALL"`
 }{}
 
+// Read the configuration file and create directory if not exists
+// or set default values if values are missing
+func validateConfiguration() {
+
+	Debug("Checking if the directories needed for the program exists")
+	if IsValueEmpty(Config.CORE.BASEDIR) {
+		Warn("BASE_DIR parameter missing in the config file, setting to default")
+		Config.CORE.BASEDIR = "/home/gpadmin/"
+	}
+
+	// App name
+	if IsValueEmpty(Config.CORE.APPLICATIONNAME) {
+		Warn("APPLICATION_NAME parameter missing in the config file, setting to default")
+		Config.CORE.APPLICATIONNAME = "gpdbinstall"
+	}
+
+	// Temp Directory
+	if IsValueEmpty(Config.CORE.TEMPDIR) {
+		Warn("TEMP_DIR parameter missing in the config file, setting to default")
+		Config.CORE.TEMPDIR = "/temp/"
+	}
+
+	// Download Directory
+	if IsValueEmpty(Config.DOWNLOAD.DOWNLOADDIR) {
+		Warn("DOWNLOAD_DIR parameter missing in the config file, setting to default")
+		Config.DOWNLOAD.DOWNLOADDIR = "/download/"
+	}
+
+	// Env Directory
+	if IsValueEmpty(Config.INSTALL.ENVDIR) {
+		Warn("ENV_DIR parameter missing in the config file, setting to default")
+		Config.INSTALL.ENVDIR = "/env/"
+	}
+
+	// Uninstall Directory
+	if IsValueEmpty(Config.INSTALL.UNINTSALLDIR) {
+		Warn("UNINTSALL_DIR parameter missing in the config file, setting to default")
+		Config.INSTALL.UNINTSALLDIR = "/uninstall/"
+	}
+
+	// Future Reference Directory
+	if IsValueEmpty(Config.INSTALL.FUTUREREFDIR) {
+		Warn("UNINTSALL_DIR parameter missing in the config file, setting to default")
+		Config.INSTALL.FUTUREREFDIR = "/future_reference/"
+	}
+
+	// Check if the directory exists, else create one.
+	base_dir := Config.CORE.BASEDIR + Config.CORE.APPLICATIONNAME
+
+	// Temp the files to
+	TempDir :=  base_dir + Config.CORE.TEMPDIR
+	CreateDir(TempDir)
+
+	// Download the files to
+	DownloadDir :=  base_dir + Config.DOWNLOAD.DOWNLOADDIR
+	CreateDir(DownloadDir)
+
+	// Environment location
+	EnvFileDir := base_dir + Config.INSTALL.ENVDIR
+	CreateDir(EnvFileDir)
+
+	// Uninstall location
+	UninstallDir := base_dir + Config.INSTALL.UNINTSALLDIR
+	CreateDir(UninstallDir)
+
+	// Future Reference location
+	FutureRefDir := base_dir + Config.INSTALL.FUTUREREFDIR
+	CreateDir(FutureRefDir)
+
+}
+
+
 // Load the configuration file to the memory
 func config() {
+
+	// Load the configuration
 	configFile := "config.yml"
 	Debugf("Reading the configuration file and loading to the memory: %s", configFile)
 	configor.Load(&Config, configFile)
+
+	// Validate the configuration
+	validateConfiguration()
 }
--- gpdb/helpers.go
+++ gpdb/helpers.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"os"
+	"strings"
+)
+
 // Function that checks if the string is available on a array.
 func Contains(s []string, e string) bool {
 	for _, a := range s {
@@ -10,3 +15,39 @@ func Contains(s []string, e string) bool {
 	return false
 }
 
+// Check is the value is empty
+func IsValueEmpty(v string) bool {
+	if len(strings.TrimSpace(v)) == 0 {
+		return true
+	}
+	return false
+}
+
+// exists returns whether the given file or directory exists or not
+func doesFileOrDirExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+// Create directory
+func CreateDir(path string) {
+	// Check if the path or directory exists
+	exists, err := doesFileOrDirExists(path)
+	if err != nil {
+		Fatalf("Failed to check the directory status, the error: %v", err)
+	}
+	// If not exists then create one
+	if !exists {
+		Warnf("Directory \"%s\" does not exists, creating one", path)
+		err := os.MkdirAll(path, 0755)
+		if err != nil {
+			Fatalf("Failed to create the directory, the error: %v", err)
+		}
+	}
+}
